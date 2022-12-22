3da434fb34b03b97a05d709114288c4edff791ab
Robert Plante <RobertDPlante@gmail.com>
2022-12-22 08:55:02 -0500

    Moved day template and added year template.

---
+++ advent-solutions/templates/day_XX.tpl.rs
@@ -0,0 +1,14 @@
+use std::io::BufRead;
+
+pub fn part_01(_reader: Option<impl BufRead>) {
+    unimplemented!()
+}
+
+pub fn part_02(_reader: Option<impl BufRead>) {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+}
