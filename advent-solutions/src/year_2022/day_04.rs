a5afb0397825068358a9ebb6565ebcc7b455d596
Robert Plante <RobertDPlante@gmail.com>
2022-12-23 17:21:21 -0500

    Solutions for 2022-04

---
+++ advent-solutions/src/year_2022/day_04.rs
@@ -0,0 +1,190 @@
+use std::{fmt::Debug, io::BufRead, str::FromStr};
+
+/// Simple type to capture the start and end of a 1D range
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct SectionRange<Idx>(Idx, Idx);
+
+impl<Idx> SectionRange<Idx>
+where
+    Idx: PartialEq + PartialOrd + Copy,
+{
+    /// Determines if this `SectionRange` inclusively contains the given value
+    fn contains(&self, value: Idx) -> bool {
+        value >= self.0 && value <= self.1
+    }
+
+    /// Determines if this `SectionRange` inclusively contains the other `SectionRange`
+    fn contains_range(&self, other: &Self) -> bool {
+        self.0 <= other.0 && self.1 >= other.1
+    }
+
+    /// Determines if this `SectionRange` inclusively overlaps the other `SectionRange`
+    fn overlaps(&self, other: &Self) -> bool {
+        self.contains(other.0)
+            || self.contains(other.1)
+            || other.contains(self.0)
+            || other.contains(self.1)
+    }
+}
+
+/// A `SectionRange` for this problem is represented by two numbers separated with a hyphen
+impl<D, I> TryFrom<&str> for SectionRange<I>
+where
+    D: Debug,
+    I: FromStr<Err = D>,
+{
+    type Error = &'static str;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        value
+            .split_once("-")
+            .map(|(start, end)| {
+                SectionRange(
+                    start.parse().expect("Range start should be a number"),
+                    end.parse().expect("Range end should be a number"),
+                )
+            })
+            .ok_or("Provided str does not represent a range")
+    }
+}
+
+/// A convenient iterator adapter to create pairs of `SectionRange`s from lines containing a
+/// comma-separated string equivalent
+struct RangePairs<S, I>(I)
+where
+    S: AsRef<str>,
+    I: Iterator<Item = S>;
+
+impl<S, I> Iterator for RangePairs<S, I>
+where
+    S: AsRef<str>,
+    I: Iterator<Item = S>,
+{
+    type Item = (SectionRange<u32>, SectionRange<u32>);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|l| {
+            l.as_ref()
+                .split_once(",")
+                .map(|(l, r)| {
+                    (
+                        SectionRange::try_from(l)
+                            .expect("first part of line should represent a range"),
+                        SectionRange::try_from(r)
+                            .expect("second part of line should represent a range"),
+                    )
+                })
+                .expect("line should be two ranges separated by a comma")
+        })
+    }
+}
+
+/// Helper trait to add the method `as_range_pairs` to any `Iterator` that it applies to. This
+/// method converts the `Iterator` into one that outputs pairs of `SectionRange`s instead.
+trait AsRangePairs<S: AsRef<str>>: Iterator<Item = S> {
+    fn as_range_pairs(self) -> RangePairs<S, Self>
+    where
+        Self: Sized,
+    {
+        RangePairs(self)
+    }
+}
+
+impl<S: AsRef<str>, T: Sized> AsRangePairs<S> for T where T: Iterator<Item = S> {}
+
+pub fn part_01(reader: Option<impl BufRead>) {
+    let contain_count: u32 = reader
+        .expect("data should be available for this problem")
+        .lines()
+        .flatten()
+        .as_range_pairs()
+        .map(|(l, r)| u32::from(l.contains_range(&r) || r.contains_range(&l)))
+        .sum();
+
+    println!(
+        "Number of assignment pairs where one fully contains the other: {}",
+        contain_count
+    );
+}
+
+pub fn part_02(reader: Option<impl BufRead>) {
+    let overlap_count: u32 = reader
+        .expect("data should be available for this problem")
+        .lines()
+        .flatten()
+        .as_range_pairs()
+        .map(|(l, r)| u32::from(l.overlaps(&r)))
+        .sum();
+
+    println!(
+        "Number of assignment pairs where one overlaps the other: {}",
+        overlap_count
+    );
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn parse_ranges() {
+        let input = r#"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"#;
+
+        let ranges = input.lines().as_range_pairs().collect::<Vec<_>>();
+
+        assert_eq!(
+            vec![
+                (SectionRange(2, 4), SectionRange(6, 8)),
+                (SectionRange(2, 3), SectionRange(4, 5)),
+                (SectionRange(5, 7), SectionRange(7, 9)),
+                (SectionRange(2, 8), SectionRange(3, 7)),
+                (SectionRange(6, 6), SectionRange(4, 6)),
+                (SectionRange(2, 6), SectionRange(4, 8))
+            ],
+            ranges
+        );
+    }
+
+    #[test]
+    fn range_contains() {
+        let ranges = vec![
+            (SectionRange(2, 4), SectionRange(6, 8)),
+            (SectionRange(2, 3), SectionRange(4, 5)),
+            (SectionRange(5, 7), SectionRange(7, 9)),
+            (SectionRange(2, 8), SectionRange(3, 7)),
+            (SectionRange(6, 6), SectionRange(4, 6)),
+            (SectionRange(2, 6), SectionRange(4, 8)),
+        ];
+
+        let full_overlaps = ranges
+            .iter()
+            .map(|(l, r)| l.contains_range(r) || r.contains_range(l))
+            .collect::<Vec<_>>();
+
+        assert_eq!(vec![false, false, false, true, true, false], full_overlaps);
+    }
+
+    #[test]
+    fn range_overlaps() {
+        let ranges = vec![
+            (SectionRange(2, 4), SectionRange(6, 8)),
+            (SectionRange(2, 3), SectionRange(4, 5)),
+            (SectionRange(5, 7), SectionRange(7, 9)),
+            (SectionRange(2, 8), SectionRange(3, 7)),
+            (SectionRange(6, 6), SectionRange(4, 6)),
+            (SectionRange(2, 6), SectionRange(4, 8)),
+        ];
+
+        let partial_overlaps = ranges
+            .iter()
+            .map(|(l, r)| l.overlaps(r))
+            .collect::<Vec<_>>();
+
+        assert_eq!(vec![false, false, true, true, true, true], partial_overlaps);
+    }
+}
--- advent-solutions/src/year_2022/mod.rs
+++ advent-solutions/src/year_2022/mod.rs
@@ -1,3 +1,3 @@
 use advent_macros::generate_year;
 
-generate_year!(2022 3);
+generate_year!(2022 4);
