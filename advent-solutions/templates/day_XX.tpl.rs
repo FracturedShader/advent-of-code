6947fe6518e2ef0343fe1275b5a559bc65f6d402
Robert Plante <robertdplante@gmail.com>
2024-07-29 14:16:59 -0500

    Adding clippy ignores to template since it cannot detect the meta constraint.

--- advent-solutions/templates/day_XX.tpl.rs
+++ advent-solutions/templates/day_XX.tpl.rs
@@ -1,9 +1,11 @@
 use std::io::BufRead;
 
+#[allow(clippy::needless_pass_by_value)]
 pub fn part_01(_reader: Option<impl BufRead>) {
     unimplemented!()
 }
 
+#[allow(clippy::needless_pass_by_value)]
 pub fn part_02(_reader: Option<impl BufRead>) {
     unimplemented!()
 }
