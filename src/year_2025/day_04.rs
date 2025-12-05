ab33abe7ca9926d30f7695a4a2aa2fc76b8b93d3
Robert Plante <RobertDPlante@gmail.com>
2025-12-04 21:41:46 -0500

    Solutions for 2025-04

---
+++ src/year_2025/day_04.rs
@@ -0,0 +1,97 @@
+use crate::common;
+
+/// A roll at point `p` in the `grid` is considered accessible by a forklift if it has less than
+/// four rolls around it (including diagonally).
+fn is_accessible_roll(p: glam::IVec2, grid: &common::Grid2<u8>) -> bool {
+    grid.valid_neighbors8(p)
+        .filter(|(_, (_, n))| **n == b'@')
+        .count()
+        < 4
+}
+
+/// Counts the number of rolls that can be accessed by a forklift in the current `grid`
+/// configuration.
+fn directly_accessible_rolls(grid: &common::Grid2<u8>) -> usize {
+    grid.enumerate()
+        .filter(|(p, v)| **v == b'@' && is_accessible_roll(*p, grid))
+        .count()
+}
+
+/// Repeatedly removes accessible rolls from `grid` until no more accessible rolls remain. Returns
+/// the total number of rolls removed.
+fn removable_rolls(mut grid: common::Grid2<u8>) -> usize {
+    let mut known_rolls: Vec<_> = grid
+        .enumerate()
+        .filter_map(|(p, v)| if *v == b'@' { Some(p) } else { None })
+        .collect();
+
+    let mut removed = 0;
+
+    loop {
+        let (removable, rest): (Vec<_>, Vec<_>) = known_rolls
+            .into_iter()
+            .partition(|p| is_accessible_roll(*p, &grid));
+
+        if removable.is_empty() {
+            break;
+        }
+
+        removed += removable.len();
+
+        known_rolls = rest;
+
+        for p in removable {
+            *grid.get_mut(p) = b'.';
+        }
+    }
+
+    removed
+}
+
+pub fn part_01() {
+    let grid = common::Grid2::try_from_reader(common::puzzle_input("2025-04").unwrap()).unwrap();
+    let accessible = directly_accessible_rolls(&grid);
+
+    println!("Accessible rolls: {accessible}");
+}
+
+pub fn part_02() {
+    let grid = common::Grid2::try_from_reader(common::puzzle_input("2025-04").unwrap()).unwrap();
+    let removed = removable_rolls(grid);
+
+    println!("Removed rolls: {removed}");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn data() -> &'static str {
+        r#"..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@."#
+    }
+
+    #[test]
+    fn max_three_neighbors() {
+        let grid = common::Grid2::try_from_bytes(data().bytes()).unwrap();
+        let accessible = directly_accessible_rolls(&grid);
+
+        assert_eq!(accessible, 13);
+    }
+
+    #[test]
+    fn total_removable() {
+        let grid = common::Grid2::try_from_bytes(data().bytes()).unwrap();
+        let removed = removable_rolls(grid);
+
+        assert_eq!(removed, 43);
+    }
+}
--- src/year_2025/mod.rs
+++ src/year_2025/mod.rs
@@ -1,8 +1,9 @@
 mod day_01;
 mod day_02;
 mod day_03;
+mod day_04;
 
-pub const DAYS_SOLVED: i32 = 2;
+pub const DAYS_SOLVED: i32 = 4;
 
 pub fn run_solution(day: i32, part: i32) {
     match (day, part) {
@@ -12,6 +13,8 @@ pub fn run_solution(day: i32, part: i32) {
         (2, 2) => day_02::part_02(),
         (3, 1) => day_03::part_01(),
         (3, 2) => day_03::part_02(),
+        (4, 1) => day_04::part_01(),
+        (4, 2) => day_04::part_02(),
         _ => {
             eprintln!("Solution not implemented for {day:02}-{part:02}");
         }
